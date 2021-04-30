868e01e7ea6975571ee76c9b2b1e6b66195501b4
thorgy17 <thoams300@cox.net>
2021-04-30 01:57:16 -0400

    Comments added and footer with references added

--- MainScreen.js
+++ MainScreen.js
@@ -6,11 +6,12 @@
         submitBtn.onclick = submitAnswer;
     }
 
-
+    //Simple function that allows users to enter the site upon entering any player name but if Lebron's name is entered
+    //User will be prompted that this is the correct answer. Small addition as a mini password page before main site.
     function submitAnswer(){
         let enterSite = id("Enter")
         let userAnswer = id("input-text");
-        if(userAnswer.value == "Lebron" || "Lebron James"){
+        if(userAnswer.value == "Lebron" || userAnswer.value == "Lebron James"){
             enterSite.classList.remove('hidden');
             alert("Correct Answer!")
         }
